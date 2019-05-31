82afb4da88ee409476497b1b2c397146ae22c8d3
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-05-31 23:27:08 +0200

    Added crosshair.

---
+++ Assets/Cockpit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Cockpit : MonoBehaviour
+{
+    public Texture2D crosshairImage;
+
+    private float crosshairX;
+    private float crosshairY;
+
+    void Start()
+    {
+        crosshairX = (Screen.width / 2) - (crosshairImage.width / 2);
+        crosshairY = (Screen.height / 2) - (crosshairImage.height / 2);
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(crosshairX, crosshairY, crosshairImage.width, crosshairImage.height), crosshairImage);
+    }
+}
