5b555da3574a16c8383f2152cda718294cd8ca04
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-06-06 08:19:27 +0200

    X-wing can be destroyed, removed some dead components, addes some standard assets.

---
+++ Assets/Destructable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public int hp;
+    public ParticleSystem explosionPrefab;
+
+    private bool isDestroyed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "LaserBeam" && !isDestroyed)
+        {
+            Destroy(other.gameObject);
+
+            hp -= 10;
+            if (hp <= 0) {
+                isDestroyed = true;
+                ParticleSystem explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+                float explosionDurtion = explosion.main.duration;
+                Destroy(gameObject, 0.1f);
+                Destroy(explosion, explosionDurtion);
+            }
+        }
+    }
+}
