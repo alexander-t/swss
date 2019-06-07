f4567054454d3d48b812b0370e3c3f497232d99d
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-06-07 22:49:22 +0200

    Added one laser fire sound.

--- Assets/LaserBeamEmitter.cs
+++ Assets/LaserBeamEmitter.cs
@@ -7,11 +7,18 @@ public class LaserBeamEmitter : MonoBehaviour
     public GameObject laserBeamPrefab;
     public GameObject rightGun;
     public GameObject leftGun;
+    public AudioSource audioSource;
+    public AudioClip laserSound;
 
     private const float Cooldown = 0.2f;
 
     private bool rightGunFiring = true;
     private float nextShotTime;
+
+    void Start()
+    {
+        audioSource.clip = laserSound;    
+    }
 
     void Update()
     {
@@ -20,6 +27,7 @@ public class LaserBeamEmitter : MonoBehaviour
             Vector3 beamStartPosition = rightGunFiring ? rightGun.transform.position : leftGun.transform.position;
             GameObject laserBeam = Instantiate(laserBeamPrefab, beamStartPosition, Quaternion.identity) as GameObject;
             laserBeam.GetComponent<Beam>().target = gunFocalPoint;
+            audioSource.Play();
             rightGunFiring = !rightGunFiring;
             nextShotTime = Time.time + Cooldown;
         }        
