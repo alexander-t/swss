044d0d5936ceb5e798b7b25ffe8815cb00730f15
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-06-09 22:41:22 +0200

    Added explosion when destroying the x-wing.

--- Assets/Destructable.cs
+++ Assets/Destructable.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class Destructable : MonoBehaviour
 {
     public int hp;
     public ParticleSystem explosionPrefab;
+    public AudioSource explosionAudioSource;
 
     private bool isDestroyed = false;
 
@@ -14,11 +16,18 @@ public class Destructable : MonoBehaviour
             Destroy(other.gameObject);
 
             hp -= 10;
-            if (hp <= 0) {
+            if (hp <= 0)
+            {
                 isDestroyed = true;
                 ParticleSystem explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+                explosionAudioSource.Play();
                 float explosionDurtion = explosion.main.duration;
-                Destroy(gameObject, 0.1f);
+                GameObject model = transform.Find("Model").gameObject;
+                if (model != null) { 
+                    Destroy(model, 0.1f);
+                }
+                // Destroy the outer object with a certain delay, because it holds the explosion audio source
+                Destroy(gameObject, 5);
                 Destroy(explosion, explosionDurtion);
             }
         }
