c65d3822a6409a3a09e3c0b5d098fdff07a4bd58
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-06-12 00:25:46 +0200

    Added debris in front of player to create a more realistinc forward motion.

--- Assets/Misson.cs
+++ Assets/Misson.cs
@@ -4,10 +4,15 @@ public class Misson : MonoBehaviour
 {
     public GameObject[] enemyShips;
 
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
     void OnEnemyDestroyed()
     {
         Vector3 spawnPoint = new Vector3(-100, 0, 100);
         var xWingPrefab = enemyShips[0];
-        Instantiate(xWingPrefab, transform.position, Quaternion.identity);
+        Instantiate(xWingPrefab, spawnPoint, Quaternion.identity);
     }
 }
