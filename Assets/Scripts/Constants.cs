f0ddf9fa097e7468c6f487c56c0305cb8e0636e2
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-06-16 20:31:13 +0200

    Added working front radar.

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,5 +1,6 @@
 ﻿public static class Constants
 {
+    public const string Player = "Player";
     public const string LaserBeam = "LaserBeam";
     public const string Shield_ImpactLight = "Impact Light";
 }
---
+++ Assets/Scripts/RadarProjection.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/**
+ * Projects an object towards the player at a constant distance.
+ * The projected object is to be picked up a radar camera.
+ **/
+public class RadarProjection : MonoBehaviour
+{
+    [Tooltip("Object that will be picked up by the radar camera. ")]
+    public GameObject radarSignatureObject;
+
+    private const float DistanceToRadar = 20;
+
+    private GameObject player;
+    private Vector3 direction;
+
+    void Start()
+    {
+        player = GameObject.Find(Constants.Player);
+    }
+
+    void Update()
+    {
+        direction = transform.position - player.transform.position ;
+        direction.Normalize();
+        radarSignatureObject.transform.position = player.transform.position + direction * DistanceToRadar;
+    }
+}
