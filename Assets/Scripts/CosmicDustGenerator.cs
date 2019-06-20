b965f853f861cbd14950f615ba7595c45c926307
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-06-20 18:34:52 +0200

    Changed the cosmic dust generator to generate only 10 systems and then move them.

--- Assets/Scripts/CosmicDustGenerator.cs
+++ Assets/Scripts/CosmicDustGenerator.cs
@@ -1,14 +1,40 @@
 ﻿using UnityEngine;
 
+/**
+ * Generates a number of particle systems and then moves them around so that they always
+ * remain close to the player.
+ */
 public class CosmicDustGenerator : MonoBehaviour
 {
     public ParticleSystem cosmicDustPrefab;
+    public GameObject player;
+    public int numberOfClounds = 10;
+
+    private ParticleSystem[] dustClouds;
 
     void Start()
     {
-        for (int i = 0; i < 100; i++) {
-            Vector3 position = new Vector3(Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f));
-            Instantiate(cosmicDustPrefab, position, Quaternion.identity);
+        dustClouds = new ParticleSystem[numberOfClounds];
+        for (int i = 0; i < numberOfClounds; i++) {
+            dustClouds[i] = Instantiate(cosmicDustPrefab, PointCloseToPlayer(), Quaternion.identity, this.transform);
         }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < numberOfClounds; i++)
+        {
+            if (Random.Range(0, 100) < 0.001f) {
+                dustClouds[i].transform.position = PointCloseToPlayer();
+            }
+        }
+    }
+
+    private Vector3 PointCloseToPlayer() {
+        Vector3 position = player.transform.position;
+        position.x += Random.Range(-100.0f, 100.0f);
+        position.y += Random.Range(-100.0f, 100.0f);
+        position.z += Random.Range(-100.0f, 100.0f);
+        return position;
     }
 }
