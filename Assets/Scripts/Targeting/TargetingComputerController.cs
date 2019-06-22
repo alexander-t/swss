e77bee48c5d8767c58ccdde192c55f0d2803720e
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-06-23 01:53:42 +0200

    Experimenteding with targeting bounding box.

--- Assets/Scripts/Targeting/TargetingComputerController.cs
+++ Assets/Scripts/Targeting/TargetingComputerController.cs
@@ -19,10 +19,15 @@ namespace Targeting
         private const float Cooldown = 0.1f;
         private float nextAcquisitionTime;
 
+        private new Camera camera;
+        private GameObject currentTarget;
+        private Rect targetBoundingBox = Rect.zero;
+
         #region Unity lifecycle
         void Awake()
         {
             targetingComputer = TargetingComputer.Instance;
+            camera = GameObject.Find("Main Camera").GetComponent<Camera>();
         }
 
         void Start()
@@ -31,7 +36,7 @@ namespace Targeting
             UpdateDisplay();        
         }
 
-        void LateUpdate()
+        void Update()
         {
             if (Input.GetKey(KeyCode.T))
             {
@@ -42,6 +47,7 @@ namespace Targeting
                 }
                 nextAcquisitionTime = Time.time + Cooldown;
             }
+            targetBoundingBox = CalcualateTargetBoundingBox(currentTarget);
         }
         #endregion
 
@@ -60,13 +66,63 @@ namespace Targeting
         #region Class methods
         private void UpdateDisplay()
         {
-            var currentTarget = targetingComputer.GetCurrentTarget();
-            followingTargetCamera.target = GameObject.Find(currentTarget.Name);
-            targetNameText.text = currentTarget.Name;
-            targetHullText.text = currentTarget.HullPoints + "";
-            targetShieldText.text = currentTarget.ShieldPoints + "";
+            var targetable = targetingComputer.GetCurrentTarget();
+            currentTarget = GameObject.Find(targetable.Name);
+            followingTargetCamera.target = currentTarget;
+            targetNameText.text = targetable.Name;
+            targetHullText.text = targetable.HullPoints + "";
+            targetShieldText.text = targetable.ShieldPoints + "";
+                        
         }
         #endregion
 
+        // The contents of this method are borrowed from here: https://answers.unity.com/questions/726412/2d-target-reticle-for-3d-object.html
+        private Rect CalcualateTargetBoundingBox(GameObject target) {
+            if (!IsTargetVisible(target)) {
+                return Rect.zero;
+            }
+
+            Bounds bounds = target.GetComponentInChildren<Renderer>().bounds;
+
+            // Find bounds (cube) corners
+            Vector3[] Corners3 = new Vector3[8];
+            Corners3[0] = new Vector3(bounds.min.x, bounds.min.y, bounds.min.z);
+            Corners3[1] = new Vector3(bounds.max.x, bounds.min.y, bounds.min.z);
+            Corners3[2] = new Vector3(bounds.max.x, bounds.max.y, bounds.min.z);
+            Corners3[3] = new Vector3(bounds.min.x, bounds.max.y, bounds.min.z);
+            Corners3[4] = new Vector3(bounds.min.x, bounds.min.y, bounds.max.z);
+            Corners3[5] = new Vector3(bounds.max.x, bounds.min.y, bounds.max.z);
+            Corners3[6] = new Vector3(bounds.max.x, bounds.max.y, bounds.max.z);
+            Corners3[7] = new Vector3(bounds.min.x, bounds.max.y, bounds.max.z);
+
+            // Find furthest screen positions of the corners
+            Vector2 Min = camera.WorldToScreenPoint(Corners3[0]);
+            Vector2 Max = Min;
+            for (int i = 1; i < Corners3.Length; i++)
+            {
+                Vector2 ScreenPos = camera.WorldToScreenPoint(Corners3[i]);
+                Min.x = Mathf.Min(Min.x, ScreenPos.x);
+                Min.y = Mathf.Min(Min.y, ScreenPos.y);
+                Max.x = Mathf.Max(Max.x, ScreenPos.x);
+                Max.y = Mathf.Max(Max.y, ScreenPos.y);
+            }
+
+            // Screen space is inverted and starts at the bottom
+            Min.y = Screen.height - Min.y;
+            Max.y = Screen.height - Max.y;
+            float Height = Min.y - Max.y;
+            return new Rect(Min.x, Max.y, Max.x - Min.x, Height);
+        }
+
+        private bool IsTargetVisible(GameObject target) {
+            Vector3 sp = camera.WorldToViewportPoint(target.transform.position);
+            return sp.z > 0 && sp.x > 0 && sp.x < 1 && sp.y > 0 && sp.y < 1;
+        }
+
+        private void OnGUI()
+        {
+            Debug.Log(GUI.depth);
+            GUI.Box(targetBoundingBox, "");
+        }
     }
 }
