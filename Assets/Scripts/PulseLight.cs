9bc48360a6db5186cca08932ff3c5e54532e9ff0
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-06-24 23:06:03 +0200

    Added point lights to x-wing engine and buoy. Made it pulse.

---
+++ Assets/Scripts/PulseLight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/**
+ * Pulses a light by modifying its intensity.
+ */ 
+[RequireComponent(typeof(Light))]
+public class PulseLight : MonoBehaviour
+{    
+    public float waitTime = 0.1f;
+
+    private bool pulsingUp = true;
+    private new Light light;
+    
+    public void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void Start()
+    {
+        light.intensity = 0;
+        StartCoroutine("Pulse");
+    }
+
+    private IEnumerator Pulse() {
+        while (true)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (pulsingUp)
+                {
+                    light.intensity += 0.1f;
+                }
+                else {
+                    light.intensity -= 0.1f;
+                }
+                yield return new WaitForSeconds(waitTime);
+            }
+            pulsingUp = !pulsingUp;
+        }
+    }
+}
+
