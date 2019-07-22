131c5a869002541ae62f54e7e0851de8cde9cd4d
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-07-22 16:05:13 +0200

    Added some sort of speed bar.

--- Assets/Cockpit.cs
+++ Assets/Cockpit.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Cockpit : MonoBehaviour
 {
-    public Text textVelocity;
-        
+    public TextMeshProUGUI textSpeed;
+    public Image bar;
+    public Color low;
+    public Color high;
+
     void OnVelocityChange(float[] velocities)
     {
-        if (textVelocity != null)
+        if (textSpeed != null)
         {
-            textVelocity.text = (int)velocities[0] + "";
+            textSpeed.text = (int)velocities[0] + "";
         }
+
+        bar.transform.localScale = new Vector3(velocities[0] / velocities[1], 1, 1);
+        bar.color = Color.Lerp(low, high, velocities[0] / velocities[1]);
     }
 }
