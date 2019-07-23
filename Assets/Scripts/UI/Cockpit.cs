c47605811219cca49ab931940a5b652eeaa4c785
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-07-23 08:39:30 +0200

    Added retry button to failed mission and placed container model in the right directory.

---
+++ Assets/Scripts/UI/Cockpit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace UI
+{
+    public class Cockpit : MonoBehaviour
+    {
+        public TextMeshProUGUI textSpeed;
+        public Image bar;
+        public Color low;
+        public Color high;
+
+        void OnVelocityChange(float[] velocities)
+        {
+            if (textSpeed != null)
+            {
+                textSpeed.text = (int)velocities[0] + "";
+            }
+
+            bar.transform.localScale = new Vector3(velocities[0] / velocities[1], 1, 1);
+            bar.color = Color.Lerp(low, high, velocities[0] / velocities[1]);
+        }
+    }
+}
