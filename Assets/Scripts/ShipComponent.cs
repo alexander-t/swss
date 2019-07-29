4c0c4db9b45d845d920eb115bc31f1c4839e9c0b
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2019-07-29 02:26:44 +0200

    The CR-60 has turrets that can be destroyed and its shield and colliders are properly set up.

---
+++ Assets/Scripts/ShipComponent.cs
@@ -0,0 +1,24 @@
+﻿using Core;
+using UnityEngine;
+
+public class ShipComponent : MonoBehaviour
+{
+    public int hullPoints = 10;
+    public GameObject visualComponent;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Constants.Tag_LaserBeam))
+        {
+            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+
+            hullPoints = Mathf.Max(hullPoints - 10, 0);
+
+            if (hullPoints <= 0)
+            {
+                visualComponent.SetActive(false);
+            }
+        }
+    }
+}
